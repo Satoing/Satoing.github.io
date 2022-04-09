d939f1bfc828287847519795ea6906255b2c77e4
sato <3524336191@qq.com>
2022-04-10 00:08:52 +0800

    Site updated: 2022-04-10 00:08:51

--- js/welcome.js
+++ js/welcome.js
@@ -1,15 +1,15 @@
 function welcome() {
-    let welcome_text = '欢迎光顾本蒟蒻的小窝~'
+    let welcome_text = 'Ciallo!✨'
     if (document.referrer !== '') {
         let referrer = document.referrer.split("/")[2];
-        welcome_text = "欢迎你，来自" + referrer.toUpperCase() + "的用户！"; //获取用户来源域名
+        welcome_text = "欢迎光临！";
     }
     swal({
-        title: " 欢迎！",
-        text: welcome_text + '\n打开页面下方音乐以获得更佳体验！', //欢迎文本，可自行修改
-        imageUrl: "/images/avatar.jpg", //图片，自行修改位置
-        timer: 3000, //弹出时间
-        showConfirmButton: true
+        title: " Ciallo!✨",
+        text: '这里可以听到纯度100%的二次元音乐喏~♬', //欢迎文本，可自行修改
+        imageUrl: "https://cloud.fullcomb.top/private/source/image/avatar.JPG", //图片，自行修改位置
+        showConfirmButton: true,
+        showCancelButton: true
     });
 }
 $(document).ready(() => { //若未引用JQuery，请引用
